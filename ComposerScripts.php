64cdc890271fda27a01ec06f05a85955e4253364
Fabian Bentz <info@fabianbentz.de>
2021-09-23 16:24:04 +0200

    chore: add package-json.log

--- ComposerScripts.php
+++ ComposerScripts.php
@@ -138,7 +138,6 @@ class ComposerScripts
             $io,
             $lockFile,
             $composer->getInstallationManager(),
-            $composer->getRepositoryManager(),
             $composerJson
         );
         $lockData = $locker->getLockData();
